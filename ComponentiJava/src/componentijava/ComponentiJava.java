37d9bcb63cde89a4d7f09b5bc2639b927e92f09f
Lorenzo Carfagna <lorenzo.carfagna@gmail.com>
2015-09-30 17:18:33 +0200

    Primo commit

---
+++ ComponentiJava/src/componentijava/ComponentiJava.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package componentijava;
+import java.awt.*;
+import javax.swing.*;
+/**
+ *
+ * @author Lorenzo
+ */
+public class ComponentiJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //To be implemented..
+        
+        
+    }
+    
+}
